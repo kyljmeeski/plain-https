95910eb8ca86255c26c74770eee4e933b168e294
kyljmeeski <kyljmeeski@gmail.com>
2024-05-06 17:44:40 +0600

    feat: implement execute() and containing() methods

--- src/main/java/com/kyljmeeski/plainhttps/request/PostRequest.java
+++ src/main/java/com/kyljmeeski/plainhttps/request/PostRequest.java
@@ -1,6 +1,14 @@
 package com.kyljmeeski.plainhttps.request;
 
 import com.kyljmeeski.plainhttps.*;
+import com.kyljmeeski.plainhttps.response.PlainResponse;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class PostRequest implements Request, RequestWithBody {
 
@@ -31,13 +39,41 @@ public class PostRequest implements Request, RequestWithBody {
 
     @Override
     public Response execute() {
-//        todo: implement it
-        return null;
+        headers.add("Host", host);
+        headers.add("Connection", "close");
+
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        try (SSLSocket socket = (SSLSocket) factory.createSocket(host, 443)) {
+            OutputStream output = socket.getOutputStream();
+            output.write(requestLine.bytes());
+            output.write(headers.bytes());
+            output.write("\r\n".getBytes());
+            if (body != null) {
+                output.write(body.content());
+            }
+            output.flush();
+            InputStream input = socket.getInputStream();
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            int nRead;
+            byte[] data = new byte[1024];
+            while ((nRead = input.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+            output.close();
+            input.close();
+            return new PlainResponse(buffer.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Request containing(Body body) {
-        return null;
+        headers.add("Content-Type", body.type());
+        headers.add("Content-Length", String.valueOf(body.content().length));
+        this.body = body;
+        return this;
     }
 
 }
