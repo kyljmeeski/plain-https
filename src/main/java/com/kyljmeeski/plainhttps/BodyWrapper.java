d20fae59874e47e9f10aafb42e309dbeaa508bbb
kyljmeeski <kyljmeeski@gmail.com>
2024-05-08 20:12:12 +0600

    doc: add javadocs to MissingFieldException and BodyWrapper

--- src/main/java/com/kyljmeeski/plainhttps/BodyWrapper.java
+++ src/main/java/com/kyljmeeski/plainhttps/BodyWrapper.java
@@ -28,14 +28,30 @@ class GenericDeserializer<T> implements JsonDeserializer<T> {
 
 }
 
+/**
+ * Wrapper class for handling HTTP response bodies.
+ */
 public class BodyWrapper {
 
     private final byte[] content;
 
+    /**
+     * Constructs a new BodyWrapper with the specified body content.
+     *
+     * @param body the HTTP response body to wrap
+     */
     public BodyWrapper(Body body) {
         content = body.content();
     }
 
+    /**
+     * Deserializes the wrapped HTTP response body into an object of the specified class.
+     *
+     * @param <T>   the type of the object to deserialize to
+     * @param clazz the class of the object to deserialize to
+     * @return an object of the specified class representing the deserialized body content
+     * @throws MissingFieldException if a required field is missing during deserialization
+     */
     public <T> T content(Class<T> clazz) throws MissingFieldException {
         try {
             GsonBuilder gsonBuilder = new GsonBuilder();
--- src/main/java/com/kyljmeeski/plainhttps/MissingFieldException.java
+++ src/main/java/com/kyljmeeski/plainhttps/MissingFieldException.java
@@ -1,7 +1,15 @@
 package com.kyljmeeski.plainhttps;
 
+/**
+ * Exception thrown when a required field is missing during deserialization.
+ */
 public class MissingFieldException extends Exception {
 
+    /**
+     * Constructs a new MissingFieldException with the specified error message.
+     *
+     * @param message the error message
+     */
     public MissingFieldException(String message) {
         super(message);
     }
