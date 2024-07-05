00ec0d9eeea926a2888a182a627b488106d1009d
kyljmeeski <kyljmeeski@gmail.com>
2024-07-05 23:08:38 +0600

    feat: implement code() and phrase() methods of Status class, that retrieves status code and status phrase

--- src/main/java/com/kyljmeeski/plainhttps/Status.java
+++ src/main/java/com/kyljmeeski/plainhttps/Status.java
@@ -1,5 +1,7 @@
 package com.kyljmeeski.plainhttps;
 
+import java.util.Arrays;
+
 public class Status {
 
     private final byte[] status;
@@ -9,10 +11,32 @@ public class Status {
     }
 
     public int code() {
+        int start = -1;
+        for (int i = 0; i < status.length; i++) {
+            if (status[i] == 32) {
+                if (start != -1) {
+                    byte[] bytes = Arrays.copyOfRange(status, start + 1, i);
+                    return ((bytes[0] - '0') * 100) + ((bytes[1] - '0') * 10) + (bytes[2] - '0');
+                } else {
+                    start = i;
+                }
+            }
+        }
         return 0;
     }
 
     public String phrase() {
+        int start = -1;
+        for (int i = 0; i < status.length; i++) {
+            if (status[i] == 32) {
+                if (start != -1) {
+                    byte[] bytes = Arrays.copyOfRange(status, i + 1, status.length);
+                    return new String(bytes);
+                } else {
+                    start = i;
+                }
+            }
+        }
         return null;
     }
 
